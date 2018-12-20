d452ecb8eba91139245b66bc89602a1b266b2b7c
phantom <chenliangxingji1@163.com>
2018-12-20 10:40:18 +0800

    2018 12 20

--- src/main/java/annotation/util/AnnotationUtil.java
+++ src/main/java/annotation/util/AnnotationUtil.java
@@ -8,26 +8,26 @@ public class AnnotationUtil {
 	@Test("ni hao")
 	private String str= "test";
 	
-	public static String getAnnotationValue(Field field,Class< ? extends Annotation> annotationClass) {
-		String result = null;
-		Annotation[] values = field.getAnnotationsByType(annotationClass);
-		for(Annotation annotation : values) {
-			if(annotation instanceof Test) {
-				 result = ((Test)annotation).value();
-			}
-		}
-		return result;
-	}
-	
-	public static void main(String[] args) {
-		Field[] fields = AnnotationUtil.class.getDeclaredFields();
-		
-		for(Field field:fields) 
-		{
-			String value = AnnotationUtil.getAnnotationValue(field, Test.class);
-			if(value != null)System.out.println(value);
-		}
-	}
-	
+//	public static String getAnnotationValue(Field field,Class< ? extends Annotation> annotationClass) {
+//		String result = null;
+//		Annotation[] values = field.getAnnotationsByType(annotationClass);
+//		for(Annotation annotation : values) {
+//			if(annotation instanceof Test) {
+//				 result = ((Test)annotation).value();
+//			}
+//		}
+//		return result;
+//	}
+//	
+//	public static void main(String[] args) {
+//		Field[] fields = AnnotationUtil.class.getDeclaredFields();
+//		
+//		for(Field field:fields) 
+//		{
+//			String value = AnnotationUtil.getAnnotationValue(field, Test.class);
+//			if(value != null)System.out.println(value);
+//		}
+//	}
+//	
 	
 }
