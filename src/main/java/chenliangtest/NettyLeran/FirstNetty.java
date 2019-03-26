ea4b1364b3d49ffec3cbc810a2bc7f719b09ab70
phantom <chenliangxingji1@163.com>
2019-03-26 15:14:44 +0800

    remove

--- src/main/java/chenliangtest/NettyLeran/FirstNetty.java
+++
@@ -1,5 +0,0 @@
-package chenliangtest.NettyLeran;
-
-public class FirstNetty {
-	
-}
---
+++ src/main/java/xml/parse/Dom4jParseUtilForPinnacleProxy.java
@@ -0,0 +1,56 @@
+package xml.parse;
+
+import java.util.Iterator;
+
+import org.dom4j.Attribute;
+import org.dom4j.Document;
+import org.dom4j.Element;
+
+import io.netty.handler.codec.mqtt.MqttMessageBuilders.PublishBuilder;
+import xml.util.Dom4jUtil;
+
+public class Dom4jParseUtilForPinnacleProxy
+{
+   
+   public static boolean isCC(String xml) {
+      boolean result = false;
+      Document document = Dom4jUtil.parse(xml);
+      if(document !=null) {
+         Element root = document.getRootElement();
+         for (Iterator<Element> it = root.elementIterator(); it.hasNext();) {
+            Element element = it.next();
+            if("cc".equals(element.getName())){
+               result = true;
+            }
+         }
+      }
+      return result;
+   }
+   
+   public static void main(String[] args)
+   {
+      String xml1 = "<?xml version=\"1.0\"?>\r\n" + 
+               "<msg>\r\n" + 
+               "<cc id=\"3\" type=\"set\">\r\n" + 
+               "<serverTime>20160825123533</serverTime>\r\n" + 
+               "<firmware size=\"5361739\"/>\r\n" + 
+               "</cc>\r\n" + 
+               "</msg>\r\n";
+      Document document = Dom4jUtil.parse(xml1);
+      Element root  = document.getRootElement();
+      for (Iterator<Element> it = root.elementIterator(); it.hasNext();) {
+         Element element = it.next();
+         System.out.println(element.getName());
+         System.out.println(element.getData());
+         int i = element.attributeCount();
+         for(int j = 0;j<i;j++) {
+            Attribute attr = element.attribute(j);
+            System.out.println(attr.getName()+";"+attr.getData());
+         }
+         // do something
+     }
+    
+      
+   }
+   
+}
