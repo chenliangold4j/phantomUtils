dd7711aaa674eae1e49da3566d0eb04d8c423e0c
phantom <chenliangxingji1@163.com>
2019-03-26 17:19:00 +0800

    3.26 done

--- src/main/java/annotation/util/Test.java
+++ src/main/java/annotation/util/Test.java
@@ -1,6 +1,5 @@
 package annotation.util;
 
-import static java.lang.annotation.ElementType.TYPE;
 import static java.lang.annotation.RetentionPolicy.RUNTIME;
 
 import java.lang.annotation.ElementType;
