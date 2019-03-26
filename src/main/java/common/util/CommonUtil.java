9f99c04fe47cf5d5e953fa894891f623c8a95e34
phantom <chenliangxingji1@163.com>
2019-03-26 16:02:59 +0800

    test

--- src/main/java/common/util/CommonUtil.java
+++ src/main/java/common/util/CommonUtil.java
@@ -2,8 +2,8 @@ package common.util;
 
 public class CommonUtil
 {
-   static boolean isNotNull(Object object) {
-      
+   static boolean isNotNull(Object object) 
+   {
      return object == null ? false:true;
    }
 }
